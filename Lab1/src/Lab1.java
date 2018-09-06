43ad9c120ebe0483a52465460c691932f038de9d
phoney <crickonalt1@gmail.com>
2018-09-05 22:05:46 -0400

    Lab1

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Lab1 {
 	/**
 	 * @author Matthew "crickon" Grillo
 	 */
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter two numbers to find the sum: ");
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.printf("The sum of %d and %d is %d!", x, y, sum(x,y));
 		
 	}
+
+	/**
+	 * Method to find the sum of two numbers
+	 * @param x number 1
+	 * @param y number 2
+	 * @return the sum of x and y
+	 */
+	public static int sum(int x, int y) {
+		return x + y;
+	}
 }
-// Hello there this is for the first lab
