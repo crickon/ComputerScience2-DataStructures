a6470101dfba6aa97b6367910da0ebd09f60eaf5
phoney <crickonalt1@gmail.com>
2018-09-10 22:11:44 -0400

    version 1

--- Lab Ch15 Choosing Words/src/RamblecsDictionary.java
+++ Lab Ch15 Choosing Words/src/RamblecsDictionary.java
@@ -1,5 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Scanner;
+
 public class RamblecsDictionary {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+		String inputpath = "words.txt";
+		File file = new File(inputpath);
+		Scanner textReader = null;
+		try {
+			textReader = new Scanner(file);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		String outputpath = "output.txt";
+		Writer writer = null;
+		try {
+			writer = new FileWriter(outputpath, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		PrintWriter output = new PrintWriter(writer);
+
+		while (textReader.hasNextLine()) {
+			String line = textReader.nextLine();
+			if (line.length() >= 3 && line.length() <= 5) {
+				line = line.toUpperCase();
+				output.println(line);
+			}
+		}
+
+		textReader.close();
+		output.close();
 	}
 }
