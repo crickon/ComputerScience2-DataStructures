9a073c6476d3ad1ae167d179200c86d6715a1f4c
annemariecabs <caballeroa1@bishopireton.org>
2018-10-11 12:49:40 -0400

    minor metadata

--- Assignment Two/src/CardTest.java
+++ Assignment Two/src/CardTest.java
@@ -2,7 +2,15 @@ public class CardTest {
 	public static void main(String args[]) {
 		try {
 			Deck deck = new Deck();
-			Deck[] hands = deck.deal(2,2);
+			System.out.println(deck.toString());
+			
+			Deck deck2 = new Deck(false);
+			System.out.println(deck2.toString());
+			
+			deck2.shuffle();
+			System.out.println(deck2.toString());
+			
+			Deck[] hands = deck.deal(14, 4);
 			
 			for (Deck d : hands) {
 				System.out.println(d.toString());
