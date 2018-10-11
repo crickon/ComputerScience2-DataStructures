0609370b093ca6010930d1941535c808eba996b5
notaphoney <crickonalt1@gmail.com>
2018-10-11 11:35:51 -0400

    Card comparator

--- Assignment Two/src/CardComparator.java
+++ Assignment Two/src/CardComparator.java
@@ -1,16 +1,32 @@
 import java.util.Comparator;
 
 public class CardComparator implements Comparator {
+	private boolean ascending;
+
+	public CardComparator() {
+		this(true);
+	}
+
+	public CardComparator(boolean ascending) {
+		this.ascending = ascending;
+	}
 
 	public int compare(Object one, Object two) {
 		if (one instanceof Card && two instanceof Card) {
 			Card first = (Card) one;
 			Card second = (Card) two;
 
-			if (first.getRank() > second.getRank())
-				return 1;
-			else if (first.getRank() < second.getRank())
-				return -1;
+			if (ascending) {
+				if (first.getRank() > second.getRank())
+					return 1;
+				else if (first.getRank() < second.getRank())
+					return -1;
+			} else {
+				if (first.getRank() < second.getRank())
+					return 1;
+				else if (first.getRank() > second.getRank())
+					return -1;
+			}
 			return 0;
 		} else
 			return 0;
